ed747b5dd688ebfea5860d720d0beadc2ba9e8cb
SerbinY <yevgenserbin@gmail.com>
2021-01-13 00:09:22 +0200

    Автотест с выбором языка версия 3

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_add_button(browser):
     browser.get(link)
     time.sleep(10)
-    assert browser.find_element_by_css_selector("[type='submit']")
+    assert browser.find_element_by_css_selector("btn-add-to-basket")
 
