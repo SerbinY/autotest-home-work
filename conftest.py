2fd0bddc9bcf3a6b026ee93644c5a14f447ae8af
SerbinY <yevgenserbin@gmail.com>
2021-01-12 23:15:35 +0200

    Автотест с выбором языка версия 2

--- conftest.py
+++ conftest.py
@@ -6,13 +6,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 
+
 def pytest_addoption (parser):
     parser.addoption("--language", action='store', default=None, help=("Choose language"))
 
 
 @pytest.fixture(scope="function")
-def browser (reqest):
-    user_language = reqest.config.getoption("language")
+def browser (request):
+    user_language = request.config.getoption("language")
 
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
@@ -20,7 +21,7 @@ def browser (reqest):
     yield browser
     browser.quit()
 
-#    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+
 
 
 
--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
+import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("[type='submit']")
+    time.sleep(10)
+    assert browser.find_element_by_css_selector("[type='submit']")
+
